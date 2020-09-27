1d0c0a0eaa399d9c6df1113aa2ecbf28dfee30b0
Lasse Haverinen <lasse.haverinen@gmail.com>
2020-09-27 22:10:24 +0300

    Added example on multiple images

--- components/imageUpload.js
+++ components/imageUpload.js
@@ -8,14 +8,31 @@ router.get('/', (req, res) => {
     res.send("Only POST method accepted with multipart file");
 })
 
-router.post('/', multerUpload.single('testFile'), (req, res) => {    
+router.post('/', multerUpload.single('testFile'), (req, res) => {
     console.log(req.file);
 
     fs.rename(req.file.path, './uploads/' + req.file.originalname, function (err) {
         if (err) throw err;
         console.log('renamed complete');
         res.send("Test");
-      });    
+      });
+});
+
+router.post('/multiple', multerUpload.array('testFiles', 4), (req, res) => {
+  console.log(req.files);
+
+  req.files.forEach(f => {
+    fs.renameSync(f.path, './uploads/' + f.originalname)
+  })
+
+  res.send("Completed");
+  /*
+  fs.rename(req.file.path, './uploads/' + req.file.originalname, function (err) {
+      if (err) throw err;
+      console.log('renamed complete');
+      res.send("Test");
+    });    */
+
 });
 
 module.exports = router;
